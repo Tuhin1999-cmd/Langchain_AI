50697148e8172b359b92d2db68f8318a3d5ca808
StratFord Lab <stratfordlabs@gmail.com>
2026-03-01 03:25:44 +0530

    use gemma3 in local

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
 import os
 
 from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from prompttemplate import system_prompt
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_ollama import ChatOllama
+from langchain_core.output_parsers import StrOutputParser
+from information import getinformation
+from prompttemplate import system_prompt
 
+load_dotenv()
 
 def main():
-    load_dotenv()
-    print(os.environ.get("GOOGLE_API_KEY"))
+    
+    info = getinformation()
+    
+    summary_prompt_template = PromptTemplate(input_variables = [info] , template = system_prompt)
+    
+    # llm = ChatGoogleGenerativeAI(
+    #         model="gemini-3-flash-preview",
+    #         google_api_key=os.environ.get("GOOGLE_API_KEY"),
+    #     )
 
+    llm = ChatOllama(
+        model="gemma3:270m",
+        temperature=0
+    )
+    
+    chain = summary_prompt_template | llm | StrOutputParser()
+    response = chain.invoke({
+        "info" : info
+    })
+    
+    print(response)
 
 if __name__ == "__main__":
     main()
---
+++ prompttemplate.py
@@ -0,0 +1,9 @@
+from information import getinformation
+
+info = getinformation()
+
+system_prompt = """
+ given a person information {info} i want to create 
+ 1. a short summary
+ 2. interesting facts
+"""
